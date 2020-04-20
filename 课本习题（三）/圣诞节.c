c44441d339335e3a6bd3f607d00e39b2c1c905b0
sh2268411762 <2268411762@qq.com>
2020-04-20 23:52:08 +0800

    。

---
+++ 课本习题（三）/圣诞节.c
@@ -0,0 +1,127 @@
+//#include "test.h"
+//
+//
+//int main()
+//{
+//	int day = 1;
+//	for (day = 1; day <= 12; day++)
+//	{
+//		switch (day)
+//		{
+//		case 1:
+//			printf("frist（第一天）\n");
+//			break;
+//
+//		case 2:
+//			printf("second（第二天）\n");
+//			break;
+//
+//		case 3:
+//			printf("third（第三天）\n");
+//			break;
+//
+//		case 4:
+//			printf("fourth（第四天）\n");
+//			break;
+//
+//		case 5:
+//			printf("fifth（第五天）\n");
+//			break;
+//
+//		case 6:
+//			printf("sixth（第六天）\n");
+//			break;
+//
+//		case 7:
+//			printf("seventh（第七天）\n");
+//			break;
+//
+//		case 8:
+//			printf("eighth（第八天）\n");
+//			break;
+//
+//		case 9:
+//			printf("ninth（第九天）\n");
+//			break;
+//
+//		case 10:
+//			printf("tenth（第十天）\n");
+//			break;
+//
+//		case 11:
+//			printf("eleventh（第十一天）\n");
+//			break;
+//
+//		case 12:
+//			printf("twelfth（第十二天）\n");
+//			break;
+//		}
+//		switch (day)
+//		{
+//		case 1:
+//			printf("my true love sent to me\n\nA partridge in a pear tree.\n\n");
+//			break;
+//
+//		case 2:
+//			printf("my true love sent to me\n\nTwo turtle doves,\n\nAnd a partridge in a pear tree!\n\n");
+//			break;
+//
+//		case 3:
+//			printf("my true love sent to me\n\nThree French hens,\n\nTwo turtle doves,\n\nAnd a partridge in a pear tree!\n\n");
+//			break;
+//
+//		case 4:
+//			printf("my true love sent to me\n\nFour calling birds,\n\nThree French hens,\n\nTwo turtle doves,\n\nAnd a partridge in a pear tree!\n\n");
+//			break;
+//
+//		case 5:
+//			printf("wu\n\n");
+//			break;
+//
+//		case 6:
+//			printf("liu\n\n");
+//			break;
+//
+//		case 7:
+//			printf("qi\n\n");
+//			break;
+//
+//		case 8:
+//			printf("ba\n\n");
+//			break;
+//
+//		case 9:
+//			printf("jiu\n\n");
+//			break;
+//
+//		case 10:
+//			printf("shi\n\n");
+//			break;
+//
+//		case 11:
+//			printf("shiyi\n\n");
+//			break;
+//
+//		case 12:
+//			printf("12\n\n");
+//			break;
+//
+//		}
+//	}
+//	return 0;
+//}
+//
+//
+//
+//
+//
+//
+//
+////
+////int main()//慎用浮点数
+////{
+////	float a = 1000000.00;
+////	a += 0.12f;
+////	printf("%.2f", a);
+////	return 0;
+////}
---
+++ 课本习题（三）/毕达哥拉斯三元组.c
@@ -0,0 +1,19 @@
+//#include "test.h"
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 500; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= 500; j++)
+//		{
+//			int k = 0;
+//			for (k = 1; k <= 500; k++)
+//			{
+//				printf("%d*%d+%d*%d=%d*%d\n", i, i, j, j, k, k);
+//			}
+//		}
+//	}
+//	return 0;
+//}
